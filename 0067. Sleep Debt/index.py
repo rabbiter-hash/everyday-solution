8a39968223255c72b4c46d329cbafe6aac234f79
rabbiter-hash <vipdentfy@outlook.com>
2026-05-19 17:37:12 +0800

    Get sleep debt from input array and target sleep hours

--- 0067. Sleep Debt/index.py
+++ 0067. Sleep Debt/index.py
@@ -7,19 +7,19 @@
 # @Motto: ABC(Always Be Coding)
 
 def sleep_debt(hours_slept, target_hours):
-    # 1. 变量设定
-    # 1.1、过去几天的睡觉小数总和
-    past_total = sum(hours_slept)
-    # 1.2、过去几天的天数
-    n = len(hours_slept)
-    # 1.3、总共需要睡觉的小时数，包含今天
-    need_total = target_hours * (n + 1)
+	# 1. 变量设定
+	# 1.1、过去几天的睡觉小数总和
+	past_total = sum(hours_slept)
+	# 1.2、过去几天的天数
+	n = len(hours_slept)
+	# 1.3、总共需要睡觉的小时数，包含今天
+	need_total = target_hours * (n + 1)
 
-    # 2. 判定
-    # 如果过去睡觉的总数已经超过需要的小时数，按照题目意思返回0
-    if past_total >= need_total:
-        return 0
+	# 2. 判定
+	# 如果过去睡觉的总数已经超过需要的小时数，按照题目意思返回0
+	if past_total >= need_total:
+		return 0
 
-    return need_total - past_total
+	return need_total - past_total
 
 print(sleep_debt([6, 6, 6, 6, 6, 6], 8))
